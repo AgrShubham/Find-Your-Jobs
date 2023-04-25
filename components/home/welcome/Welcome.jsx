626c927263aa375cbf6b6b46b936aa0faa63627e
Shubham Agrawal <0808cs211165.ies@ipsacademy.org>
2023-04-25 14:44:01 +0530

    first commit

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,28 @@
+import {useState} from 'react'
+import { 
+  View, 
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList
+} from 'react-native'
+import {useRouter} from 'expo-router';
+
+import styles from './welcome.style'
+import {icon, SIZES } from '../../../constants';
+
+const Welcome = () => {
+  const router =useRouter();
+  
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Shubham</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>    
+    </View>
+  )
+}
+
+export default Welcome
